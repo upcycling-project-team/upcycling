ae84acdb6da252af8b36471d35ecb6e239409ada
dlrldyd <osamuelo@naver.com>
2024-01-23 18:12:41 +0900

    css.html했고 js랑 약간의 수정이 필요함

--- src/main/java/com/example/upcycling/controller/FaqController.java
+++ src/main/java/com/example/upcycling/controller/FaqController.java
@@ -3,9 +3,28 @@ package com.example.upcycling.controller;
 import com.example.upcycling.service.FaqService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/faq")
 public class FaqController {
     private final FaqService faqService;
+
+    @GetMapping("/faq01")
+    public String faq01(){
+        return "faq/faq01";
+    }
+
+    @GetMapping("/faq02")
+    public String faq02(){
+        return "faq/faq02";
+    }
+
+    @GetMapping("/faq03")
+    public String faq03(){
+        return "faq/faq03";
+    }
+
 }
--- src/main/java/com/example/upcycling/controller/ShopReviewController.java
+++ src/main/java/com/example/upcycling/controller/ShopReviewController.java
@@ -3,9 +3,22 @@ package com.example.upcycling.controller;
 import com.example.upcycling.service.ShopReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/shop")
 public class ShopReviewController {
     private final ShopReviewService shopReviewService;
+
+    @GetMapping("/shop01")
+    public String shop01(){
+        return "shop/shop01";
+    }
+
+    @GetMapping("/shop02")
+    public String shop02(){
+        return "shop/shop02";
+    }
 }
