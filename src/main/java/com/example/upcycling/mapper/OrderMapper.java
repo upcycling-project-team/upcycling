6c3890a8b9ac37ebe3d9a39513e61da0bba03a9d
shindongeok <ehrflqakstp@gmail.com>
2024-01-23 18:12:35 +0900

    주문내역 처리중

--- src/main/java/com/example/upcycling/mapper/OrderMapper.java
+++ src/main/java/com/example/upcycling/mapper/OrderMapper.java
@@ -1,8 +1,12 @@
 package com.example.upcycling.mapper;
 
+import com.example.upcycling.domain.vo.MypageInquiryVo;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface OrderMapper {
+
+//    주문상품 조회
+//    Long selectOrderinquiry(MypageInquiryVo mypageInquiryVo);
 
 }
