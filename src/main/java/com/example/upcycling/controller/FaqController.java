ebfa73751df2d1cee4ee44a13c61c5d8c9bb8f8b
dlrldyd <osamuelo@naver.com>
2024-01-25 18:12:22 +0900

    faq검색기능을했다.

--- src/main/java/com/example/upcycling/controller/FaqController.java
+++ src/main/java/com/example/upcycling/controller/FaqController.java
@@ -3,8 +3,13 @@ package com.example.upcycling.controller;
 import com.example.upcycling.service.FaqService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+//실험중
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -12,22 +17,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FaqController {
     private final FaqService faqService;
 
-    @GetMapping("/faq01")
+    @GetMapping("/main")
     public String faq01(){
         return "faq/faq01";
     }
 
-    @GetMapping("/faq02")
-    public String faq02(){
+    @GetMapping("/search-result")
+    public String faq02(String keyword, Model model){
+        System.out.println("keyword = " + keyword);
+        List<String> faqTitleList = faqService.findSearch(keyword);
+
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("titleList", faqTitleList);
+
         return "faq/faq02";
     }
 
-    @GetMapping("/faq03")
+    @GetMapping("/detail")
     public String faq03(){
         return "faq/faq03";
     }
 
-    @GetMapping("/faq04")
+    @GetMapping("/write")
     public String faq04(){
         return "faq/faq04";
     }
